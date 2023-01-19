5cdd4d2c525b5d8a61c8cd3be97274d4919b61c1
IllIA2107 <illia.karmazin@vtsd.com>
2023-01-19 11:56:29 -0500

    1

---
+++ Triangle/Main.java
@@ -0,0 +1,14 @@
+package Triangle;
+
+public class Main {
+    static double sides[] = new double[3];
+
+    public static void main(String args[]){
+
+        sides[1] = Input.inputSides(1);
+        sides[2] = Input.inputSides(2);
+        sides[3] = Input.inputSides(3);
+
+
+    }
+}
